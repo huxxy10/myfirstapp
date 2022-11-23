e8beb1c7820e333103303eae527e099384baa56b
hbturaki <hbturaki@gmail.com>
2022-11-23 12:29:02 +0100

    changes

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ActiveRecord::Base 
-	validates :title, presence: true, length:{minimum:5, maximum:20}
-	validates :description, presence: true, length:{minimum:10, maximum:20}
+	belongs_to :user
+	validates :title, presence: true, length:{minimum:5, maximum:50}
+	validates :description, presence: true, length:{minimum:10, maximum:200}
 
 end 
