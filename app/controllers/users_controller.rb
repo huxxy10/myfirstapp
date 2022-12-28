99f60aa308dc09d82257b325568e4c09cab60993
hbturaki <hbturaki@gmail.com>
2022-12-28 15:22:01 +0100

    show profile pic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController 
+def index 
+@users = User.all
+end 
 
  def new 
  @user = User.new
  end 
+
     def create 
 @user = User.new(user_params)
 if @user.save
@@ -12,9 +16,11 @@ if @user.save
 		render 'new'
 	end 
 end 
+
 def edit 
 @user = User.find(params[:id])
 	end 
+
 	def update  
 @user = User.find(params[:id])
 if @user.update(user_params)
@@ -23,10 +29,12 @@ if @user.update(user_params)
 	else 
 		render 'edit'
 	end 
-
-	end 
+end 
+def show
+@user = User.find(params[:id])
+end 
 	  private 
 	              def user_params
-		         params.require(:user).permit(:username, :email, :password)
-               end
-end 
+		         params.require(:user).permit(:username,:email,:password)
+    end
+		end 
